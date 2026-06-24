19e7bf7b7bf19da864c9fdff121e8d033fef00a9
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-24 14:23:42 +0200

    feat(ex01): add a function change the value to many pointers

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 14:04:54 by nfurst            #+#    #+#             */
+/*   Updated: 2026/06/24 14:21:16 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*
+int		main(void)
+{
+	int n;
+	int *ptr1 = &n;
+	int **ptr2 = &ptr1;
+	int ***ptr3 = &ptr2;
+	int ****ptr4 = &ptr3;
+	int *****ptr5 = &ptr4;
+	int ******ptr6 = &ptr5;
+	int *******ptr7 = &ptr6;
+	int ********ptr8 = &ptr7;
+	int *********ptr9 = &ptr8;
+
+	ft_ultimate_ft(ptr9);
+
+	printf("%d", n);
+
+	return (0);
+}
+*/
