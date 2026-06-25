5d1bd9c77fa4c1ae79cd4b2656aa1dc880da027b
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-25 15:00:00 +0200

    feat(ex07): add a function to reverse interger array

---
+++ ex07/ft_rev_int_tab.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/25 14:37:11 by nfurst            #+#    #+#             */
+/*   Updated: 2026/06/25 14:58:38 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = size - 1;
+	while (i < j)
+	{
+		temp = tab[i];
+		tab[i] = tab[j];
+		tab[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+/*
+int main(void)
+{
+	int arr[] = {1, 2, 3, 4, 5};
+
+	int *ptr = arr;
+
+	ft_rev_int_tab(ptr, 5);
+	return 0;
+}
+*/
