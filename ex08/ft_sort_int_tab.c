faaa4fc9f80ec4df892b0c650460d884f1fb26ca
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-26 14:44:43 +0200

    refactor(ex08): fix formatting and change for loop to while loop

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,31 +6,34 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 15:01:49 by nfurst            #+#    #+#             */
-/*   Updated: 2026/06/25 15:34:03 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/06/26 14:43:46 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
-	for (int i = 0; i < size -1; i++)
-	{
-		int	min;
+	int	i;
+	int	j;
+	int	min;
+	int	temp;
 
+	i = 0;
+	while (i < size)
+	{
 		min = i;
-		for (int j = i + 1; j < size; j++)
+		j = i + 1;
+		while (j < size)
 		{
 			if (tab[j] < tab[min])
-			{
 				min = j;
-			}
+			j++;
 		}
 		if (min != i)
 		{
-			int temp;
-
 			temp = tab[min];
 			tab[min] = tab[i];
 			tab[i] = temp;
 		}
+		i++;
 	}
 }
